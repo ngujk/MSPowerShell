e75b1a31dd01c9a621bb563841bb9bc402c130db
ngujk <ngujacks@sheridancollege.ca>
2026-03-16 20:04:28 -0400

    Delete midterm5.ps1

--- midterm5.ps1
+++
@@ -1,7 +0,0 @@
-Get-Process
-$processes = Get-Process
-$processes
-Get-Process | Where-Object {$_.CPU -gt 5000}
-Get-Process | Sort-Object WorkingSet64 -Descending 
-$processes | Where-Object {$_.CPU -gt 5000}
-$processes | Sort-Object WorkingSet64 -Descending 
