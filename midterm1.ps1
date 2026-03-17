5d286ecf27bd514ce06e2a7ad23830380073b7cc
ngujk <ngujacks@sheridancollege.ca>
2026-03-16 20:03:39 -0400

    Delete midterm1.ps1

--- midterm1.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem env:
-$env:COMPUTERNAME
-$env:USERNAME
-$path = Read-Host -Prompt 'Enter a path to search for large files'
-$rawFileData = Get-ChildItem -Path $path -Recurse
-$largeFiles = $rawFileData | Where-Object {$_.Length -gt 100MB}
-$largeFilesCount = $largeFiles | Measure-Object | Select-Object -ExpandProperty Count
-Write-Host "You have $largeFilesCount large file(s) in $path"
